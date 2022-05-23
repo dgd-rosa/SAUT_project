55c0288f8cebe062c469191d0c7f966abe8d57c3
dgd-rosa <dgd.rosa@gmail.com>
2022-05-23 15:36:15 +0000

    classe

--- ekf_saut/ekf.py
+++ ekf_saut/ekf.py
@@ -1,6 +1,25 @@
-import numpy
+import numpy as np
+from abc import ABC, abstractmethod
 
 
-class EKF:
-    def __init__(self):
-        
+class EKF(ABC):
+    def __init__(self, u, v ,r, psi):
+        self.u = u
+        self.v = v
+        self.r = r
+        self.psi = psi
+    
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
+
+class EKF_simple(EKF):
+
+    def update()
+        x_dot = np.cos(self.psi) - sin(self.psi)
+        y_dot = np.sin(self.psi) + np.cos(self.psi)
+        psi_dot = r
