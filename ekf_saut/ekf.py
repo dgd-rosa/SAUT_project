24d5bbac7e82611541ca062636df2b2ca4a7f29a
dgd-rosa <dgd.rosa@gmail.com>
2022-05-27 13:58:48 +0000

    cenas no ekf

--- ekf_saut/ekf.py
+++ ekf_saut/ekf.py
@@ -21,9 +21,9 @@ class EKF(ABC):
         pass
 
 class EKF_simple(EKF):
-    def __init__(self, state_dimension, process_mean, process_variance, measurement_mean, measurement_variance):
+    def __init__(self, state_dimension, process_mean, process_variance, measurement_mean, measurement_variance, altitude):
         self.state_dimension = state_dimension
-        
+        self.altitude = altitude # it is always constant
         self.current_apriori_state = [0] * state_dimension
         self.current_state = [0] * state_dimension
 
@@ -44,25 +44,42 @@ class EKF_simple(EKF):
     def R_matrix(self):
         tmp_vect = np.full(shape=self.state_dimension, fill_value = (random.gauss(self.measurement_mean, self.process_variance))**2, dtype=np.float)
         return np.diag(tmp_vect)
+    
+    def H_matrix(self, x_beacon, y_beacon, psi, b, e):
+        x_k = self.current_apriori_state[0]
+        y_k = self.current_apriori_state[1]
+        aux_x = (x_k - x_beacon)/np.sqrt((x_k-x_beacon)**2 + (y_k-y_beacon)**2 + self.altitude**2) # we consider z_beacon=0
+        aux_y = (y_k - y_beacon)/np.sqrt((x_k-x_beacon)**2 + (y_k-y_beacon)**2 + self.altitude**2) # we consider z_beacon=0
+        return np.array([np.cos(-e)*np.cos(-b-psi)*aux_x , np.cos(-e)*np.cos(-b-psi)*aux_y], [np.cos(-e)*np.sin(-b-psi)*aux_x , np.cos(-e)*np.sin(-b-psi)*aux_y])
 
-    def kalman_gain(self):
-        pass
+    #TODO ver ruído pois ruido = v R v^T
+    def kalman_gain(self, H, R):
+        return self.P_priori @ np.transpose(H) @ np.inverse(H @ self.P_priori @ np.transpose(H) + R)
 
-    def predict(self, u, v, r, b, e, psi):
+    def predict(self, u, v, psi, t_step):
         # Compute the predicted state
-        self.current_apriori_state[0] = self.current_state[0] + u*np.cos(psi) - v*np.sin(psi) + r*np.cos(-e)*np.cos(-b-psi)
-        self.current_apriori_state[1] = self.current_state[1] + u*np.sin(psi) + v*np.cos(psi) + r*np.cos(-e)*np.sin(-b-psi)
+        self.current_apriori_state[0] = self.current_state[0] + u*t_step*np.cos(psi) - v*t_step*np.sin(psi)
+        self.current_apriori_state[1] = self.current_state[1] + u*t_step*np.sin(psi) + v*t_step*np.cos(psi)
 
         # Compute the a priori error covariance estimate
-        self.P_priori = self.A_matrix() * self.P_priori * np.transpose(self.A_matrix()) + self.Q_matrix()
+        self.P_priori = self.A_matrix() @ self.P_priori @ np.transpose(self.A_matrix()) + self.Q_matrix()
 
     #TODO: Ended my last work here in update function
-    def update(self, x_beacon, y_beacon):
-        kalman_gain = self.kalman_gain()
+    def update(self, x_beacon, y_beacon, psi, b, e):
+        x_k = self.current_apriori_state[0]
+        y_k = self.current_apriori_state[1]
+        
+        h_x = x_beacon + np.cos(-e)*np.cos(-b-psi)*np.sqrt((x_k-x_beacon)**2 + (y_k-y_beacon)**2 + self.altitude**2) # we consider z_beacon=0
+        h_y = y_beacon + np.cos(-e)*np.sin(-b-psi)*np.sqrt((x_k-x_beacon)**2 + (y_k-y_beacon)**2 + self.altitude**2) # we consider z_beacon=0
+        h = np.array([z_x], [z_y])
+        
+        K = self.kalman_gain(self.H_matrix(x_beacon, y_beacon, psi, b, e), self.R_matrix())
+        #x_k+1 = x_apriori + K(z - h(x_k))
+        self.current_state = self.current_apriori_state + K*(z - )
 
     # Beacon X and Y as input args, as these could be later dynamic... for now we consider the beacon as always in the same position
-    def compute_iteration(self, x_beacon, y_beacon, u, v, r, b, e, psi):
-        self.predict(u, v, r, b, e, psi)
+    def compute_iteration(self, x_beacon, y_beacon, u, v, r, b, e, psi, t_step):
+        self.predict(u, v, r, b, e, psi, t_step)
         self.update(x_beacon, y_beacon)
         print(self.current_state)
 
